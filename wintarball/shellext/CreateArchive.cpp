fc9546a4272321176df3e06932f3d95513b37561
aegis <aegis@f05a4d3f-5a97-4418-b679-a456eae28545>
2001-10-22 16:16:49 +0000

    Oops - forgot the blank end-of-tar header
    git-svn-id: file:///desktop/SourceForge%20Backup/WinTarBall/svnrepos/trunk@18 f05a4d3f-5a97-4418-b679-a456eae28545

--- wintarball/shellext/CreateArchive.cpp
+++ wintarball/shellext/CreateArchive.cpp
@@ -248,6 +248,10 @@ bool CreateArchive(
         }
     }
 
+    // write the blank block
+    memset(&b, 0, sizeof(b));
+    out->Write(b.buffer, 1, TAR_BLOCK_SIZE);
+
     // if the user cancelled, try to remove the file
     if (cancelled) {
         delete out.release();
